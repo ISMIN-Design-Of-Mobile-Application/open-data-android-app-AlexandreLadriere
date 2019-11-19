20b9814855f914765411390f351c7b62325cb1d0
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2019-11-19 17:29:39 +0100

    fix: Fixing schema warning for DB by adding exportSchema = false to SportDatabase and PlaceDatabase

--- app/src/main/java/com/ismin/opendataapp/sportsfragment/database/SportDatabase.kt
+++ app/src/main/java/com/ismin/opendataapp/sportsfragment/database/SportDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [SportEntity::class], version = 1)
+@Database(entities = [SportEntity::class], version = 1, exportSchema = false)
 abstract class SportDatabase : RoomDatabase() {
 
     abstract fun getSportDAO(): SportDAO
