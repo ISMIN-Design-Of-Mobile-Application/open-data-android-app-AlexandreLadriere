9a5e114060ae10b894d5bbd5d3c01810267d450f
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2019-11-24 10:19:06 +0100

    add: When the user clicks on a marker title, the place details activity is started

--- app/src/main/java/com/ismin/opendataapp/MapFragment.kt
+++ app/src/main/java/com/ismin/opendataapp/MapFragment.kt
@@ -1,6 +1,7 @@
 package com.ismin.opendataapp
 
 import android.content.Context
+import android.content.Intent
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
@@ -15,15 +16,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.ismin.opendataapp.placesfragment.database.PlaceEntity
 
 class MapFragment : Fragment(), GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback {
 
     private var listener: OnFragmentInteractionListener? = null
     private lateinit var gMap: GoogleMap
     private var locationsList: MutableMap<String, LatLng> = mutableMapOf()
+    private var placeEntityList: MutableMap<String, PlaceEntity> = mutableMapOf()
     private var isMapReady = false
-
-    //TODO: detect if whether or not the localisation is on
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,12 +40,15 @@ class MapFragment : Fragment(), GoogleMap.OnInfoWindowClickListener, OnMapReadyC
     override fun onMapReady(googleMap: GoogleMap) {
         gMap = googleMap
         isMapReady = true
-        gMap.setOnInfoWindowClickListener(this)
-
     }
 
     override fun onInfoWindowClick(p0: Marker?) {
         Toast.makeText(context, p0?.title.toString(), Toast.LENGTH_LONG).show()
+        val intent = Intent(context, PlaceDetailsActivity::class.java)
+        if(p0?.tag != null) {
+            intent.putExtra(Intent.EXTRA_TEXT, p0?.tag as PlaceEntity)
+            this.startActivity(intent)
+        }
     }
 
     override fun onAttach(context: Context) {
@@ -74,12 +78,14 @@ class MapFragment : Fragment(), GoogleMap.OnInfoWindowClickListener, OnMapReadyC
     private fun displayOnMap() {
         gMap.clear()
         for ((key, location) in locationsList) {
-            gMap.addMarker(MarkerOptions().position(location).title(key))
+            gMap.addMarker(MarkerOptions().position(location).title(key)).tag = placeEntityList[key]
         }
+        gMap.setOnInfoWindowClickListener(this)
     }
 
-    fun addLocation(location: Location, name: String) {
+    fun addLocation(location: Location, name: String, placeEntity: PlaceEntity) {
         locationsList[name] = LatLng(location.latitude, location.longitude)
+        placeEntityList[name] = placeEntity
         if (isMapReady) {
             displayOnMap()
         }
@@ -88,6 +94,7 @@ class MapFragment : Fragment(), GoogleMap.OnInfoWindowClickListener, OnMapReadyC
     fun setActualLocation(location: Location) {
         val key = "Current Location"
         locationsList[key] = LatLng(location.latitude, location.longitude)
+        placeEntityList[key] = PlaceEntity(0, key, "Here", "0", location.latitude.toString(), location.longitude.toString())
         if (isMapReady) {
             displayOnMap()
         }
